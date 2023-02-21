41aa1f62ef56458d1994b41fe1bb999fea2d07c1
delioth <blmenke@gmail.com>
2023-02-20 21:47:55 -0600

    fix names

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 function registerSettings() {
 	// Create the setting for disabling/re-enabling the popup.
-	game.settings.register("foundry-pf2e-vault-weapon-masters", "popupVis", {
+	game.settings.register("foundry-pf2e-vaulted-weapon-masters", "popupVis", {
 		name: "One-Time Popup",
 		scope: "client",
 		hint: "Renables the popup displayed when the module was first activated, will force a reload to immediately present the pop up, useful if you need to retrive the bug report URL ",
@@ -17,15 +17,15 @@ Hooks.once("init", () => {
 });
 
 async function openIntroduction (){
-	const journals = await game.packs.get("foundry-pf2e-vault-weapon-masters.vault-weapon-masters-journals").getDocuments();
+	const journals = await game.packs.get("foundry-pf2e-vaulted-weapon-masters.vaulted-weapon-masters-journals").getDocuments();
 	const introduction = journals.filter(e => e.id == "CGM7KN8WMUMcHSl1")[0]
 	introduction.sheet.render(true)
-	game.settings.set("foundry-pf2e-vault-weapon-masters", "popupVis", false)
+	game.settings.set("foundry-pf2e-vaulted-weapon-masters", "popupVis", false)
 }
 
 Hooks.once('ready', async function () {
     if (game.user.isGM) {
-        if (game.settings.get("foundry-pf2e-vault-weapon-masters", "popupVis") == true) {
+        if (game.settings.get("foundry-pf2e-vaulted-weapon-masters", "popupVis") == true) {
             let d = new Dialog({
                 title: "Vaulted Weapon Masters Activated",
 
@@ -35,7 +35,7 @@ Hooks.once('ready', async function () {
 								</p>
 								<p>For a summary of your new content click the Introduction button below</p>
 								<p>
-								This Foundry VTT module has been prepared by Braden "Delioth" Menke of Eldritch Osiris Games, please report any bugs <a href="https://github.com/Eldritch-Osiris-Games/foundry-pf2e-vault-weapon-masters/issues">here!</a>
+								This Foundry VTT module has been prepared by Braden "Delioth" Menke of Eldritch Osiris Games, please report any bugs <a href="https://github.com/Eldritch-Osiris-Games/foundry-pf2e-vaulted-weapon-masters/issues">here!</a>
 								</p>
                 `,
                 buttons: {
@@ -47,7 +47,7 @@ Hooks.once('ready', async function () {
                     two: {
                         icon: '<i class="fas fa-times-circle"></i>',
                         label: "Close",
-                        callback: () => game.settings.set("foundry-pf2e-vault-weapon-masters", "popupVis", false)
+                        callback: () => game.settings.set("foundry-pf2e-vaulted-weapon-masters", "popupVis", false)
                     },
                 },
             },{ width: 450});
